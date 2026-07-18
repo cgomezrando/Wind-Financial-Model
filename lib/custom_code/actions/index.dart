b4a47a06cb951563cacd2436935de718b0e9a92d
cgomezrando <cgomezrando@gmail.com>
2026-07-18 09:59:19 +0200

    Delete lib directory

--- lib/custom_code/actions/index.dart
+++
@@ -1,2 +0,0 @@
-export '/custom_code/actions/show_loading_dialog.dart' show showLoadingDialog;
-export '/custom_code/actions/show_error_dialog.dart' show showErrorDialog;
--- lib/index.dart
+++
@@ -1,9 +0,0 @@
-// Export pages
-export '/pages/cash_flows_page/cash_flows_page_widget.dart'
-    show CashFlowsPageWidget;
-export '/pages/debt_schedule_page/debt_schedule_page_widget.dart'
-    show DebtSchedulePageWidget;
-export '/pages/assumptions_page/assumptions_page_widget.dart'
-    show AssumptionsPageWidget;
-export '/pages/results_dashboard_page/results_dashboard_page_widget.dart'
-    show ResultsDashboardPageWidget;
